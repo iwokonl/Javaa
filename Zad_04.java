528f4ad0feca6b77beb38ea46389856b2a4b4cd1
Iwo Stanisławski <37576228+iwokonl@users.noreply.github.com>
2023-01-08 14:09:47 +0100

    Update Zad_04.java

--- Zad_04.java
+++ Zad_04.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.util.Scanner;
 import java.math.BigInteger;
+
 public class Main {
     public static int countChar(String str, char c) {
         int ilosc = 0;
@@ -179,10 +180,10 @@ public class Main {
         System.out.print("plik: " + plik(literka) + "\n");
         System.out.print("plik2: " + plik(str2) + "\n");
         BigInteger big = new BigInteger(String.valueOf(1));
-        for(int i = 0;i<2*4;i++){
+        for (int i = 0; i < 2 * 4; i++) {
             big = big.multiply(new BigInteger("2"));
         }
-        System.out.print("Ziarenka maku: "+big);
+        System.out.print("Ziarenka maku: " + big);
         BigDecimal k = new BigDecimal("2000");
         BigDecimal jedne = new BigDecimal("1");
         BigDecimal sto = new BigDecimal("100");
